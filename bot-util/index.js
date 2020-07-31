c1fa69248cabb4fb672603c32200dcb81306e6f9
Jonathan Zhao <mac@Macs-MacBook-Pro.local>
2020-07-30 17:25:55 -0700

    removed faulty imports from bot util index

--- bot-util/index.js
+++ bot-util/index.js
@@ -1,12 +1,3 @@
-const Entities = require('html-entities').Html5Entities
-const Discord = require('discord.js')
-const Emojis = require('./emojis')
-const Streams = require('./streams')
-const Config = require('./config')
-const GuildSettings = require('./guildSettings')
-const UserSettings = require('./userSettings')
-const Common = require('./common')
-
 module.exports = {
   Embeds: require('./embeds'),
   Responses: require('./responses'),
