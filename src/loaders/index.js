b9bc1be6f77f9c511a5adb95e4d0f71887fd898c
Jonathan Zhao <mac@Macs-MacBook-Pro.local>
2020-08-07 21:28:06 -0700

    Moved all source code and resources into src, added new Dotenv.js file for dotenv

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -4,13 +4,13 @@ const Keyv = require('./Keyv')
 const WebApis = require('./WebApis')
 const Process = require('./Process')
 const Paths = require('./Paths')
+const Dotenv = require('./Dotenv')
 const Common = require('../bot-util/Common')
-const env = require('dotenv').config()
-if (env.error) throw env.error
 
 module.exports = function (client) {
-  Discord(client, Common)
   Paths(Common)
+  Dotenv(Common)
+  Discord(client, Common)
   Logger(Common)
   Keyv(Common)
   Process(Common)
